73e123740adf040af63a9f95a8d2ca1687d67ce1
Sharon Lee <sharonlee714@gmail.com>
2019-11-01 11:15:23 -0700

    update files

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-  $('form#user').submit(function(event)) {
+  $("form#user").submit(function(event) {
+    console.log("begin execution");
     event.preventDefault;
 
-    var input = parseInt($("#input1")).val();
+    var input = $("#input1").val(); // example: 5
+    console.log("this is the input" + input);
     var output = [];
-    var loopNumbers = [i];
 
-    for(i = 0; i <= input; i++)
+    for(var i = 0; i <= input; i++) {
+      var loopNumbers = i; // ex: loop through 1 - 5
+      console.log(loopNumbers);
+      if (loopNumbers === 3) {
+        output[loopNumbers] = "I'm sorry Dave, I'm afraid I can't do that";
+      } else if (loopNumbers.indexOf("2") > -1) {
+        output[loopNumbers] = "boop";
+      } else if (loopNumbers.indexOf("1") > -1) {
+        output[loopNumbers] = "beep";
+      } else {
+        output[loopNumbers] = loopNumbers;
+      }
+    }
 
-    if (loopNumbers.indexOf(3) > -1) {
-
+    // return output;
+    $(".output").text(output);
   });
 });
