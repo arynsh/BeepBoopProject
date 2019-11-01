fc5cc24a1f2d9ebecef1624ade5076443f606bf1
Sharon Lee <sharonlee714@gmail.com>
2019-11-01 16:28:59 -0700

    changes to scripts to separate business/user logic plus changes to html

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,7 @@ $(document).ready(function() {
   $("form#user").submit(function(event) {
 
     var input = $("#input1").val();
-    var output = [];
-
-      for(var i = 0; i <= input; i++) {
-        var loopNumbers = i.toString();
-        console.log(loopNumbers);
-
-        if (loopNumbers.indexOf("3") > -1) {
-          output[i] = "I'm sorry Dave, I'm afraid I can't do that";
-          console.log(output[i]);
-        } else if (loopNumbers.indexOf("2") > -1) {
-          output[i] = "boop";
-        } else if (loopNumbers.indexOf("1") > -1) {
-          output[i] = "beep";
-        } else {
-          output[i] = loopNumbers;
-        }
-      }
+    var output = numberReplacer(input);
 
     $(".output").text(output);
     $(".outputReverse").text(output.reverse());
@@ -26,3 +10,23 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
+function numberReplacer(input) {
+  var output = [];
+  for(var i = 0; i <= input; i++) {
+    var loopNumbers = i.toString();
+    console.log(loopNumbers);
+
+    if (loopNumbers.indexOf("3") > -1) {
+      output[i] = "I'm sorry Dave, I'm afraid I can't do that";
+      console.log(output[i]);
+    } else if (loopNumbers.indexOf("2") > -1) {
+      output[i] = "boop";
+    } else if (loopNumbers.indexOf("1") > -1) {
+      output[i] = "beep";
+    } else {
+      output[i] = loopNumbers;
+    }
+  }
+  return output;
+}
