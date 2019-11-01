1fd3d07a0f2e8fc63b51167ca34b06f0c75d1a7e
Sharon Lee <sharonlee714@gmail.com>
2019-11-01 15:13:37 -0700

    add .toString to index to change numbers into string so indexOf method may be called.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
   $("form#user").submit(function(event) {
-    console.log("begin execution");
 
-    var input = $("#input1").val(); // example: 5
+    var input = $("#input1").val();
     var output = [];
+    var outputReverse = outPut.reverse();
 
-    for(var i = 0; i <= input; i++) {
-      var loopNumbers = i; // ex: loop through 1 - 5
+      for(var i = 0; i <= input; i++) {
+        var loopNumbers = i.toString();
 
-      if (loopNumbers.indexOf("3") > -1) {
-        output[i] = "I'm sorry Dave, I'm afraid I can't do that";
-      } else if (loopNumbers.indexOf("2") > -1) {
-        output[i] = "boop";
-      } else if (loopNumbers.indexOf("1") > -1) {
-        output[i] = "beep";
-      } else {
-        output[i] = loopNumbers;
+        if (loopNumbers.indexOf("3") > -1) {
+          output[i] = "I'm sorry Dave, I'm afraid I can't do that";
+        } else if (loopNumbers.indexOf("2") > -1) {
+          output[i] = "boop";
+        } else if (loopNumbers.indexOf("1") > -1) {
+          output[i] = "beep";
+        } else {
+          output[i] = loopNumbers;
+        }
       }
-    }
+      return output;
+
 
     $(".output").text(output);
+    $(".outputReverse").text(outputReverse);
+
     event.preventDefault();
   });
 });
