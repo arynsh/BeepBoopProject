0c6a53d0c4923464dc0eae766625e5224c591b4f
Sharon Lee <sharonlee714@gmail.com>
2019-11-01 15:54:08 -0700

    add working version of program due to deletion of 'return output'

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,14 @@ $(document).ready(function() {
 
     var input = $("#input1").val();
     var output = [];
-    var outputReverse = outPut.reverse();
 
       for(var i = 0; i <= input; i++) {
         var loopNumbers = i.toString();
+        console.log(loopNumbers);
 
         if (loopNumbers.indexOf("3") > -1) {
           output[i] = "I'm sorry Dave, I'm afraid I can't do that";
+          console.log(output[i]);
         } else if (loopNumbers.indexOf("2") > -1) {
           output[i] = "boop";
         } else if (loopNumbers.indexOf("1") > -1) {
@@ -18,11 +19,9 @@ $(document).ready(function() {
           output[i] = loopNumbers;
         }
       }
-      return output;
-
 
     $(".output").text(output);
-    $(".outputReverse").text(outputReverse);
+    $(".outputReverse").text(output.reverse());
 
     event.preventDefault();
   });
