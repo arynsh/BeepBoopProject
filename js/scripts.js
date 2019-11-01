2c006ee49f20a2006f4d2ddb1ca9a21217b77156
Sharon Lee <sharonlee714@gmail.com>
2019-11-01 10:09:39 -0700

    add start of scripts.js file plus README.md

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('form#user').submit(function(event)) {
+    event.preventDefault;
+
+    var input = parseInt($("#input1")).val();
+    var output = [];
+    var loopNumbers = [i];
+
+    for(i = 0; i <= input; i++)
+
+    if (loopNumbers.indexOf(3) > -1) {
+
+  });
+});
